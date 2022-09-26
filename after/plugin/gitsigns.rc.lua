0c30d3124157bc5718efcdb886ecb30ab2325303
terrnit <terrnit.megaman@gmail.com>
2022-09-27 04:15:00 +0500

    gitsigns: map for current line (not toggle)

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -11,6 +11,6 @@ git.setup {
 }
 
 
-v.nvim_set_keymap('n', 'gb', ':Gitsigns toggle_current_line_blame<Return>', { silent = true } )
+v.nvim_set_keymap('n', 'gb', ':Gitsigns blame_line<Return>', { silent = true } )
 
 
